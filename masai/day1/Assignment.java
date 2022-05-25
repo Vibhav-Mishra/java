7d9c0df148395619461bf7b25fb86e77dd865a02
Vibhav-Mishra <97462883+Vibhav-Mishra@users.noreply.github.com>
2022-05-25 20:50:33 +0530

    This is day1 assignment

---
+++ masai/day1/Assignment.java
@@ -0,0 +1,18 @@
+ public class Assignment {
+	public static void main (String[] args){
+		String name="vibhav";
+		String father_name="akhilesh mishra";
+		String mother_name="sunita mishra";
+		String age="23";
+		String gender="male";
+		String address="mejaroad,prayagraj,up";
+	System.out.println("name="+name);
+	System.out.println("father name="+father_name);
+	System.out.println("mother name="+mother_name);
+	System.out.println("age="+age);
+	System.out.println("gender="+gender);
+	System.out.println("address="+address);
+
+		
+	}
+}
---
+++ masai/day1/Name.java
@@ -0,0 +1,14 @@
+class Assignment {
+	Public static void main (string[] args){
+		String name="Vibhav";
+		String city="Prayagraj";
+		String age="23";
+		String gender="male";
+	System.out.println("name",+name);
+	System.out.println("city",+city);
+	System.out.println("age",+age);
+	System.out.println("gender",+gender);
+
+		
+	}
+}
